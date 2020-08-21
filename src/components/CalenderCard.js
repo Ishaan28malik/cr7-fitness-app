6fb1000e8b0a9a1356e7f23b9be800f9e0dabc28
ishaan.m <ishaan.m@tyroo.com>
2020-08-22 01:07:35 +0530

    Final UI functionality

--- src/components/CalenderCard.js
+++ src/components/CalenderCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../index.scss';
 import { HiDotsHorizontal } from 'react-icons/hi';
-import { AiOutlineMinus } from 'react-icons/ai';
 
 const CalenderCard = (type) => {
   return (
@@ -12,20 +11,16 @@ const CalenderCard = (type) => {
         <div className='calender-sub-cards'>
           <div className='calender-card-card1'>
             <div>2</div>
-            <AiOutlineMinus />
-            <div>
-              <h6>work</h6>
-            </div>
+            <hr class='solid'></hr>
+            <h6>work</h6>
           </div>
           <div className='calender-card-card2'>
             <div>3</div>
-            <AiOutlineMinus />
-            <div>
-              <h6>Personal</h6>
-            </div>
+            <hr class='solid'></hr>
+            <h6>Personal</h6>
           </div>
         </div>
-        <h6> Meeting 08:15</h6>
+        <h6>-> Meeting 08:15</h6>
         <div className='calender-dots'>
           <HiDotsHorizontal />
         </div>
