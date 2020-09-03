1fb1568fb88cb586b27281c623ad4332cef650f4
Ishaan <ishaanmalikrm7@gmail.com>
2020-09-03 14:51:06 +0530

    Updates

---
+++ precache-manifest.2e213f1599b7c15132d01630533d96c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83315fdd3376cae8487935e19cdf0a16",
+    "url": "/cr7-fitness-app/index.html"
+  },
+  {
+    "revision": "0c379c9e380f75a65ba0",
+    "url": "/cr7-fitness-app/static/css/2.943a44eb.chunk.css"
+  },
+  {
+    "revision": "c0f7f4b348d614c504b7",
+    "url": "/cr7-fitness-app/static/css/main.00763d09.chunk.css"
+  },
+  {
+    "revision": "0c379c9e380f75a65ba0",
+    "url": "/cr7-fitness-app/static/js/2.43a7cdc2.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/cr7-fitness-app/static/js/2.43a7cdc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f7f4b348d614c504b7",
+    "url": "/cr7-fitness-app/static/js/main.e63d71df.chunk.js"
+  },
+  {
+    "revision": "26e46be73963796c1faf",
+    "url": "/cr7-fitness-app/static/js/runtime-main.348b9759.js"
+  },
+  {
+    "revision": "1f6f980f71944549f0290efc8beef03a",
+    "url": "/cr7-fitness-app/static/media/IPhone-X.1f6f980f.svg"
+  },
+  {
+    "revision": "4877b5f5738e44d4b5e70a29bbd9bfe7",
+    "url": "/cr7-fitness-app/static/media/apple-watch-2.4877b5f5.png"
+  }
+]);
