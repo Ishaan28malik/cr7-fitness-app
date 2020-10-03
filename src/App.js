3b6eebebdb4f6ddbc0099b88b750fe6392096e92
Ayush Khandelwal <ayush@Ayushs-MacBook-Air.local>
2020-10-04 01:20:50 +0530

    improved totally messed css and many typos

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import useDarkMode from 'use-dark-mode';
 import Phoneview from './components/PhoneView';
 import WatchView from './components/WatchView';
 import cr7 from './assets/cr7.png'
+import { BiMoon } from "react-icons/bi";
+import { BsMoon } from "react-icons/bs";
 
 
 /* style */
@@ -23,19 +25,34 @@ import './index.scss';
 const App = (type) => {
   const darkMode = useDarkMode(false);
   return (
-    <>
-      <img alt="CR7" style={{
-        width: '60px',
-        height: '60px',
-        marginLeft: '300px',
+    <div id='mainpage'>
+      <div id="heading"style={{
+        display:'flex',
+        flexDirection: 'row'
+      }}>
+        <img alt="CR7" style={{
+        width: '8vw',
+        height: '8vw',
+        marginLeft: '1vw',
+        marginTop: '1vh',
       }} src={cr7} />
-      <h1 style={{ marginTop: '-50px' }}>CR7 Fitness App</h1>
+
+      <h1 style={{
+        color:'#4ceddd',
+        fontSize:'3vw',
+      }}>
+        CR7 Fitness App</h1>
+        <div className="navbar-right" onClick={darkMode.toggle}>
+            {darkMode.value ? <BiMoon color="white" /> : <BsMoon />}
+          </div> 
+
+        </div>
       <div className='container'>
         <LightMainCard {...darkMode} />
         <Phoneview {...darkMode}/>
         <WatchView {...darkMode}/>
       </div>
-    </>
+    </div>
   );
 };
 
