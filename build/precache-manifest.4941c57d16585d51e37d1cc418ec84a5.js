e6e89a424eaec965ad45f359e4424893167dfcce
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2020-10-04 00:58:32 +0530

    Build folder to host on Netlify

---
+++ build/precache-manifest.4941c57d16585d51e37d1cc418ec84a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5bd3a4fe832ed9103a834b69ebf5f5",
+    "url": "/cr7-fitness-app/index.html"
+  },
+  {
+    "revision": "f7c83c4c9aa9af97cdc0",
+    "url": "/cr7-fitness-app/static/css/2.aeef66da.chunk.css"
+  },
+  {
+    "revision": "bd27ee4c4d8c6835cb94",
+    "url": "/cr7-fitness-app/static/css/main.a6a598a8.chunk.css"
+  },
+  {
+    "revision": "f7c83c4c9aa9af97cdc0",
+    "url": "/cr7-fitness-app/static/js/2.bb85ba04.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/cr7-fitness-app/static/js/2.bb85ba04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd27ee4c4d8c6835cb94",
+    "url": "/cr7-fitness-app/static/js/main.12d61d9d.chunk.js"
+  },
+  {
+    "revision": "26e46be73963796c1faf",
+    "url": "/cr7-fitness-app/static/js/runtime-main.348b9759.js"
+  },
+  {
+    "revision": "4c394f5a900d96cc005ea7c8bea3ba78",
+    "url": "/cr7-fitness-app/static/media/IPhone-X.4c394f5a.svg"
+  },
+  {
+    "revision": "4877b5f5738e44d4b5e70a29bbd9bfe7",
+    "url": "/cr7-fitness-app/static/media/apple-watch-2.4877b5f5.png"
+  }
+]);
